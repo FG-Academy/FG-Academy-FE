caa9cfbfcbab2b88512357230074ff34cf69a241
speardragon <rkdckdfyyd@naver.com>
2024-05-03 16:44:08 +0900

    hotfix: git 변경

--- src/app/(home)/course/[courseId]/lib/createMarkup.ts
+++
@@ -1,8 +0,0 @@
-export function createMarkup(text: string) {
-  const paragraphs = text.split("\n\n");
-  return {
-    __html: paragraphs
-      .map((paragraph) => `<p>${paragraph.replace(/\n/g, "<br />")}</p>`)
-      .join(""),
-  };
-}
